c255f3185d3bac3aebf2418a74d5a5fffa9db426
jarpar <Mamotoja88>
2020-09-20 10:07:17 +0200

    refactored

--- src/main/java/ConnectionDemo.java
+++ src/main/java/ConnectionDemo.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 public class ConnectionDemo {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
-        Connection con = DriverManager
-                .getConnection("jdbc:mysql://localhost/baza", "user", "pass");
+        Connection connection = DriverManager
+                .getConnection("jdbc:mysql://localhost:3306/zajecia3?serverTimezone=Europe/Warsaw", "root", "root");
+        connection.close();
     }
 }
