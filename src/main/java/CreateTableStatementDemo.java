9538341d68cdda8bc7481bd071a12db92c0a62dd
jarpar <Mamotoja88>
2020-09-20 11:08:03 +0200

    refactored

--- src/main/java/CreateTableStatementDemo.java
+++ src/main/java/CreateTableStatementDemo.java
@@ -8,6 +8,13 @@ public class CreateTableStatementDemo {
         Connection connection = ConnectionDemo.getConnection();
         Statement createTable = connection.createStatement();
         createTable.execute("drop table if exists demo");
+        int rows = createTable.executeUpdate("insert into demo values" +
+                "(1,'ALA')," +
+                "(2,'BEATA')," +
+                "(3,'CZESLAW')," +
+                "(4,'DARIUSZ')," +
+                "(5,'EUGENIUSZ')"
+        );
         boolean execute = createTable.execute("create table if not exists demo(id integer primary key, name varchar(20))");
         System.out.println("Wartość zwrócona przez polecenie: " + execute);
 
