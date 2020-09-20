6c23512bbeb9572f17d02b984ac9afa30fc82a92
jarpar <Mamotoja88>
2020-09-20 12:56:12 +0200

    refactored

--- src/main/java/UpdateAndInsertDemo.java
+++ src/main/java/UpdateAndInsertDemo.java
@@ -1,10 +1,23 @@
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UpdateAndInsertDemo {
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, SQLException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
         Connection connection = ConnectionDemo.getConnection();
-
+        Statement selectAll = connection.createStatement(
+                ResultSet.TYPE_SCROLL_SENSITIVE,
+                ResultSet.CONCUR_UPDATABLE
+        );
+        ResultSet records = selectAll.executeQuery("select * from demo");
+        while ((records.next())) {
+            int points = records.getInt("points");
+            if (records.wasNull()) {
+                records.updateInt("points", 0);
+                records.updateRow();
+            }
+        }
     }
 }
