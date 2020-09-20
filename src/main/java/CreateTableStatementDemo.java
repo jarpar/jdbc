d02181c715b38c75ea6f1181f8e72fb6e7d56fb8
jarpar <Mamotoja88>
2020-09-20 10:35:59 +0200

    refactored

--- src/main/java/CreateTableStatementDemo.java
+++ src/main/java/CreateTableStatementDemo.java
@@ -1,9 +1,14 @@
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class CreateTableStatementDemo {
     public static void main(String[] args) throws IllegalAccessException, InstantiationException, SQLException, NoSuchMethodException, InvocationTargetException, ClassNotFoundException {
         Connection connection = ConnectionDemo.getConnection();
+        Statement createTable = connection.createStatement();
+        boolean execute = createTable.execute("create table if not exists demo(id integer primary key, name varchar(20))");
+        System.out.println("Wartość zwrócona przez polecenie: " + execute);
+
     }
 }
