42266fd4db4eb2a5e62ef91ef2acde69c0f29dc3
jarpar <Mamotoja88>
2020-09-26 10:26:37 +0200

    refactored

--- src/main/java/jpa/EntityManagerDemo.java
+++ src/main/java/jpa/EntityManagerDemo.java
@@ -5,5 +5,6 @@ import javax.persistence.EntityManager;
 public class EntityManagerDemo {
     public static void main(String[] args) {
         EntityManager em = MyPersistence.JAVA5.getEntityManager();
+        em.close();
     }
 }
