a17b695754eaf2b5fa07f79ab88f9b10aed0e779
jarpar <Mamotoja88>
2020-09-26 12:00:33 +0200

    refactored

--- src/main/java/entity/Question.java
+++ src/main/java/entity/Question.java
@@ -13,4 +13,13 @@ public class Question {
 
     private String body;
 
+    private String option1;
+
+    private String option2;
+
+    private String option3;
+
+    private String option4;
+
+    private int validOption;
 }
