2e17d525682a841787bd7d9319c4b26d61b787fc
jarpar <Mamotoja88>
2020-09-26 13:51:37 +0200

    refactored

--- src/main/java/nospringquiz/QuizApp.java
+++ src/main/java/nospringquiz/QuizApp.java
@@ -3,7 +3,7 @@ package nospringquiz;
 import entity.Question;
 
 public class QuizApp {
-    public static void initData() {
+    public static void initData(QuestionRepository repository) {
         Question q = Question.builder()
                 .body("Wybierz słowo kluczowe Javy")
                 .option1("Char")
@@ -13,6 +13,29 @@ public class QuizApp {
                 .validOption(3)
                 .points(5)
                 .build();
+        repository.save(q);
+
+        q = Question.builder()
+                .body("Wybierz instrukcję przyrywającą iterację")
+                .option1("switch")
+                .option2("return")
+                .option3("continue")
+                .option4("break")
+                .validOption(4)
+                .points(5)
+                .build();
+        repository.save(q);
+
+        q = Question.builder()
+                .body("Wybierz wyrażenie, które jest fałszem")
+                .option1("10 > 5 && true")
+                .option2("a\".equals(\"a\")")
+                .option3("true == false")
+                .option4("10 != 4")
+                .validOption(3)
+                .points(5)
+                .build();
+        repository.save(q);
     }
 
     public static void main(String[] args) {
