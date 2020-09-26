a8cf0768c67e9fb39d51e4d23b8951b3e4b92cd5
jarpar <Mamotoja88>
2020-09-26 14:19:34 +0200

    refactored

--- src/main/java/nospringquiz/QuizController.java
+++ src/main/java/nospringquiz/QuizController.java
@@ -1,5 +1,27 @@
 package nospringquiz;
 
-public class QuizController {
+import entity.Question;
 
+import java.util.HashMap;
+import java.util.ListIterator;
+import java.util.Map;
+
+public class QuizController {
+    Map<Question, Integer> answers = new HashMap<>();
+    private final QuizService quizService;
+    ListIterator<Question> questions;
+    Question currentQuestion;
+
+    public QuizController(QuizService quizService) {
+        this.quizService = quizService;
+        questions = quizService.getQuiz(0).listIterator();
+    }
+
+    public Question next() {
+        if (questions.hasNext()) {
+
+            currentQuestion = questions.next();
+        }
+        return currentQuestion;
+    }
 }
