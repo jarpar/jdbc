67670fdd4892474e9ab35c5db2f75901d4ed7c0d
jarpar <Mamotoja88>
2020-09-26 11:46:01 +0200

    refactored

--- src/main/java/jpa/EntityManagerDemo.java
+++ src/main/java/jpa/EntityManagerDemo.java
@@ -3,6 +3,8 @@ package jpa;
 import entity.Demo;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 public class EntityManagerDemo {
     public static void main(String[] args) {
@@ -13,9 +15,28 @@ public class EntityManagerDemo {
         Demo newDemo = new Demo();
         newDemo.setName("TESTOWY");
         newDemo.setPoints(300);
+        em.getTransaction().begin();
         em.persist(newDemo);
-        System.out.println(newDemo);
+        em.getTransaction().commit();
 
+        Query query = em.createQuery(
+                "select d.name from Demo d where d.name = :qname and d.points = :qpoints",
+                Demo.class
+        );
+        query.setParameter("qname", "TESTOWY");
+        query.setParameter("qpoints", 300);
+        List<Demo> resultList = query.getResultList();
+        for (Demo d : resultList
+        ) {
+            System.out.println(d);
+        }
+
+        em.getTransaction().begin();
+        em.remove(demo);
+        em.getTransaction().commit();
+        demo = em.find(Demo.class, 1);
+        System.out.println(demo);
         em.close();
+
     }
 }
