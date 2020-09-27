62d2c72155b4c956b644b2627c37b1cb50cc2350
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-09-27 11:50:28 +0200

    refactored

--- src/main/java/springquiz/QuizServiceJpa.java
+++ src/main/java/springquiz/QuizServiceJpa.java
@@ -1,22 +1,32 @@
 package springquiz;
 
 import entity.Question;
+import entity.Quiz;
 import nospringquiz.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizServiceJpa implements QuizService {
 
     private final QuizRepository quizRepository;
 
+    @Autowired
     public QuizServiceJpa(QuizRepository quizRepository) {
         this.quizRepository = quizRepository;
     }
 
     @Override
     public List<Question> getQuiz(long id) {
-        return null;
+        Optional<Quiz> quiz = quizRepository.findById(id);
+        if (quiz.isPresent()) {
+            return new ArrayList<>(quiz.get().getQuestions());
+        }
+        return Collections.emptyList();
     }
 }
