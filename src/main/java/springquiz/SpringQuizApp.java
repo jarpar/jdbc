ab8f4f9b430b2354186681dbd027575c374bc00d
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-09-27 12:06:59 +0200

    refactored

--- src/main/java/springquiz/SpringQuizApp.java
+++ src/main/java/springquiz/SpringQuizApp.java
@@ -1,9 +1,15 @@
 package springquiz;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
-public class SpringQuizApp {
+public class SpringQuizApp implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(SpringQuizApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 }
