f5ca2d73191f8644854afc31e70beba193926eac
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-09-27 11:48:09 +0200

    refactored

--- src/main/java/springquiz/QuizServiceJpa.java
+++ src/main/java/springquiz/QuizServiceJpa.java
@@ -2,9 +2,11 @@ package springquiz;
 
 import entity.Question;
 import nospringquiz.QuizService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class QuizServiceJpa implements QuizService {
 
     private final QuizRepository quizRepository;
