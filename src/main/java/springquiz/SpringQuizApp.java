e8d5fcf09c82c3138c538b0e51b0db8cc82cbd73
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-09-27 11:57:12 +0200

    refactored

---
+++ src/main/java/springquiz/SpringQuizApp.java
@@ -0,0 +1,9 @@
+package springquiz;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringQuizApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQuizApp.class, args);
+    }
+}
