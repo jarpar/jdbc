86b80e6fb87a11a0840bb41eac36ad657e3de324
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-09-27 09:20:38 +0200

    refactored

---
+++ src/main/java/entity/Option.java
@@ -0,0 +1,11 @@
+package entity;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Option {
+    private String option1;
+    private String option2;
+    private String option3;
+
+}
